71ea70677ddea36c0b1c5a0ff14244fbd8684cb6
UkanovaM <ukanovamalika@gmail.com>
2019-10-16 04:25:15 +0600

    5

--- printcomb.go
+++ printcomb.go
@@ -5,14 +5,12 @@ import "github.com/01-edu/z01"
 func PrintComb() {
 	for i := '0'; i <= '9'; i++ {
 		z01.PrintRune(i)
-		z01.PrintRune('\n')
 		for k := i + 1; k <= '9'; k++ {
 			z01.PrintRune(k)
-			z01.PrintRune('\n')
 			for l := k + 1; l <= '9'; l++ {
 				z01.PrintRune(l)
-				z01.PrintRune('\n')
 			}
 		}
 	}
+	z01.PrintRune('\n')
 }
