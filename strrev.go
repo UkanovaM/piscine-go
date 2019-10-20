d604714c641bd8f473e732943ea985be4aeac659
UkanovaM <ukanovamalika@gmail.com>
2019-10-20 19:50:44 +0600

    str

--- strrev.go
+++ strrev.go
@@ -1,12 +1,12 @@
-package piscine45
-
+package piscine
 func StrRev(s string) string {
 
 	var reverse string
 	n:=0
 	for _,c := s {
-		if c == c
+		if c == c{
 		n ++
+	       }
 	}
 	for i := n- 1; i >= 0; i-- {
 		reverse += string(s[i])
