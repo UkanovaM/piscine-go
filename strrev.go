30f5c2308c37f53fa404b4af7076615814ed1b65
UkanovaM <ukanovamalika@gmail.com>
2019-10-20 23:20:37 +0600

    str

--- strrev.go
+++ strrev.go
@@ -2,13 +2,13 @@ package piscine
 func StrRev(s string) string {
 
 	var reverse string
-	n:=0
+	n:= 0
 	for _,c := s {
-		if c == c{
+		if c == c {
 		n ++
 	       }
 	}
-	for i := n- 1; i >= 0; i-- {
+	for i := n - 1; i >= 0; i-- {
 		reverse += string(s[i])
 	}
 	return reverse
