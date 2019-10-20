e957ba6fbe9d0ee307ea6b36c435af222c7d1738
UkanovaM <ukanovamalika@gmail.com>
2019-10-20 23:47:53 +0600

    03.09.

--- basicatoi.go
+++ basicatoi.go
@@ -1,16 +1,13 @@
 package piscine
 
-import (
-	
-)
-func BasicAtoi(s string) int
-   x := 0
-   for _,c := range s {
-	   cnt := 0
-	   for i := '1'; i <= c; i++ {
-		   cnt++
-	   }
-	   x = x*10 + cnt
-   }
-   return x 
+func BasicAtoi(s string) int {
+	x := 0
+	for _, c := range s {
+		cnt := 0
+		for i := '1'; i <= c; i++ {
+			cnt++
+		}
+		x = x*10 + cnt
+	}
+	return x
 }
