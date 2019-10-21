ad2d48b9bfa9044562888d9b6ba91f2c8005778e
UkanovaM <ukanovamalika@gmail.com>
2019-10-21 20:44:52 +0600

    04.08

--- findnextprime.go
+++ findnextprime.go
@@ -1,18 +1,20 @@
-package piscine
+package findnextprime
+
+func check(x int) bool {
+	if x <= 1 {
+		return false
+	}
+	for i := 2; i*i <= x; i++ {
+		if x%i == 0 {
+			return false
+		}
+	}
+	return true
+}
 
 func FindNextPrime(x int) int {
-	check := false
 	for {
-		if x <= 1 {
-			check = false
-		}
-		for i := 2; i*i <= x; i++ {
-			if x%i == 0 {
-				check = false
-			}
-		}
-		check = true
-		if check == true {
+		if check(x) {
 			return x
 		}
 		x++
