256d0c07385ec3cdbb29ebbfa8d386efe19d82ce
UkanovaM <ukanovamalika@gmail.com>
2019-10-21 17:39:50 +0600

    04.01

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,10 +1,11 @@
 package piscine
 
 func IterativeFactorial(nb int) int {
-	if nb >= 0 && nb <= 20 } {
+	if nb >= 0 && nb <= 20 {
 		result := 1
-		for i := 0; i <= nb; i++ 
-	}{
+	}
+
+	for i := 0; i <= nb; i++ {
 		result = result * i
 	}
 	return result
