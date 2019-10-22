c2d557714e266b21a32ced0f392d7e4e51dc1c72
UkanovaM <ukanovamalika@gmail.com>
2019-10-23 01:23:33 +0600

    hh

---
+++ basicjoin.go
@@ -0,0 +1,14 @@
+package piscine
+
+func BasicJoin(strs []string) string {
+	var a string
+	a = "Hello!"
+	var b string
+	b = "How"
+	var c string
+	c = "are"
+	var d string
+	d = "you?"
+
+	fmt.PrintRune("New string: ", a+b+c+d)
+}
