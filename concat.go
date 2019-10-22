379a0dd7214641a47ff5f2f5dcb4c718399cd8be
UkanovaM <ukanovamalika@gmail.com>
2019-10-23 01:18:13 +0600

    kll

--- concat.go
+++ concat.go
@@ -1,11 +1,5 @@
 package piscine
 
-import "fmt"
-
 func Concat(str1 string, str2 string) string {
-	var str1 string
-	var str2 string
-
-	fmt.Println("New string 1: ", str1+str2)
-
+	return str1 + str2
 }
