2d11f29b55d725c2a223696374bc4f49a801fafc
UkanovaM <ukanovamalika@gmail.com>
2019-10-23 00:52:06 +0600

    kl

--- isnumeric.go
+++ isnumeric.go
@@ -2,9 +2,8 @@ package piscine
 
 func IsNumeric(str string) bool {
 	runes := []rune(str)
-	for _, r := range runes {
-		if r
-		r < '0' && r > '9') {
+	for _, r := range str {
+		if r < '0' && r > '9' {
 			return false
 		}
 	}
