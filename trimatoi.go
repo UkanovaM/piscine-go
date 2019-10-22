813f2daef0b295056d374b23dde8ae3aa16ca3cb
UkanovaM <ukanovamalika@gmail.com>
2019-10-22 20:01:16 +0600

    05.03

--- trimatoi.go
+++ trimatoi.go
@@ -1,24 +1,27 @@
 package piscine
 
 import "github.com/01-edu/z01"
+	func check(s string) bool {
+		ok := false
 
-func TrimAtoi(s string) int {
+		for_, b := range s {
+			if c >= '0' && c <= '9' {
+				ok = true
+				break
+			}
+		}
+		return ok
+
+	}
+
+func TrimAtoi(str string) int {
 
 	a := 0
 
-	ok := false
-
-	for_, b := range str {
-		if c >= '0' && c <= '9' {
-			ok = true
-			break
-		}
-	}
-	return ok
 
 	change := false
 
-	for _, b := range s {
+	for _, b := range str {
 		if c >= '0' && c <= '9'{
 			break
 		} 
@@ -29,8 +32,8 @@ func TrimAtoi(s string) int {
 			change = false
 		}
 	}
-	if check(s) == true {
-		for _, b := range s {
+	if check(str) == true {
+		for _, b := range str {
 			cnt := 0 
 			if c >= '0' && c <= '9' {
 				for i := '1'; i <= 'c'; i++ P{
