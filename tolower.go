c580f1d5320a8d440d4b676e60b35d581785f999
UkanovaM <ukanovamalika@gmail.com>
2019-10-22 22:01:05 +0600

    10

--- tolower.go
+++ tolower.go
@@ -4,7 +4,7 @@ func ToLower(s string) string {
 	b := []rune(s)
 	for i := 0; i < len(s); i++ {
 		if b[i] >= 'A' && b[i] <= 'Z' {
-			b[i] = rune(b[i] - 32)
+			b[i] = rune(b[i] + 32)
 		}
 	}
 	return string(b)
