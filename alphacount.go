506df9433a8a2ca298c954bde8e5c924fab15880
UkanovaM <ukanovamalika@gmail.com>
2019-10-22 16:58:13 +0600

    05.01

--- alphacount.go
+++ alphacount.go
@@ -7,8 +7,8 @@ func AlphaCount(str string) int {
 	counter := 0
 
 	for letter := range str {
-		z01.AlphaCount(letter)
+		z01.PrintRune(letter)
 	}
-	z01.AlphaCount(counter)
+	z01.PrintRune(counter)
 	z01.PrintRune(str)
 }
