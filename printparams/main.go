cc7e0cdac7dacbf93c29970959462cf944f08f63
UkanovaM <53727100+UkanovaM@users.noreply.github.com>
2019-10-23 17:44:04 +0600

    Update main.go

---
+++ printparams/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	arg := os.Args
+	f := true
+	for _, a := range arg {
+		if !f {
+			for _, j := range a {
+				z01.PrintRune(j)
+			}
+			z01.PrintRune('\n')
+		}
+		ok = false
+	}
+}
