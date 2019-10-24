4f3a3c960cebbd218a664d280d7e6c3512ecbb01
UkanovaM <ukanovamalika@gmail.com>
2019-10-24 18:59:16 +0600

    dfvf

---
+++ concatparams.go
@@ -0,0 +1,15 @@
+package piscine
+
+func ConcatParams(args []string) string {
+
+	answer := ""
+
+	for a, b := range answer {
+		if a == 0 {
+			asnwer = b
+			continue
+		}
+		answer = answer + '\n' + b
+	}
+	return answer
+}
