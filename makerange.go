690a9e8f914e28ec018a2e3e1c75925615054371
UkanovaM <ukanovamalika@gmail.com>
2019-10-24 18:05:55 +0600

    ld

--- makerange.go
+++ makerange.go
@@ -1,11 +1,15 @@
 package piscine
 
 func MakeRange(min, max int) []int {
+	var answer []int
+	if min >= max {
+		return nil
+	}
 
 	answer := make([]int, max-min)
 
 	for i := min; i < max; i++ {
-		answer[i] = i
+		answer[i] = i + min
 	}
 	return answer
 }
